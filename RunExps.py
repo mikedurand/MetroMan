2f734e8df35773ab6bbbdd39feed445e734c1898
Mike Durand <durand.8@osu.edu>
2020-07-27 20:07:53 -0400

    Bug fix in ReadTruth
    Had been written to only work with hard-coded number of reaches. Now works with arbitrary number.

--- RunExps.py
+++ RunExps.py
@@ -2,14 +2,13 @@
 
 from RunExp import RunExp
 
-# fid=open("/Users/mtd/Box Sync/Analysis/SWOT/Discharge/MetroManExperiments/tstFile.txt","r")
 fid=open("RunFile.txt","r")
 infile=fid.readlines()
 
-ShowFigs=True
+ShowFigs=False
 Laterals=False
 ReCalc=True #setting to False will load the existing .pkl run data and plot
-DebugMode=False
+DebugMode=True
 
 for i in range(0,len(infile)):
     RunDir=infile[i].replace('\n','')
